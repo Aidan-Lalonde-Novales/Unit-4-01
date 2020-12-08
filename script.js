22cdcb89defdb0c59c35abaac3c6cbd4056fdcc9
Aidan Lalonde-Novales <aidan.lalonde-novales@ocsbstudent.ca>
2020-12-08 18:06:15 +0000

    fixed linter errors

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Hello World Alert
 
-alert( 'Hello, World!' );
+alert('Hello, World!')
 
 // Makes an image appear
 
-function imageAppear() {
-  document.getElementById('helloworldimage').src="images/helloworld.png";
+function imageAppear () {
+  document.getElementById('helloworldimage').src= 'images/helloworld.png'
 }
