2530a57b9bbfdb839b54b2f163d3b47dc429de08
Aidan Lalonde-Novales <aidan.lalonde-novales@ocsbstudent.ca>
2020-12-08 17:33:27 +0000

    finished the code, time to run it through the linter.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Hello World Alert
+
 alert( 'Hello, World!' );
+
+// Makes an image appear
+
+function imageAppear() {
+  document.getElementById('helloworldimage').src="images/helloworld.png";
+}
